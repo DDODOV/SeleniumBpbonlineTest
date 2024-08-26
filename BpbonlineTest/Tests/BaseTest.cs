dc36cf050e4dd84bead1dfda84180ab88416aecc
DOGOV204 <136998751+DDODOV@users.noreply.github.com>
2024-08-26 03:37:59 +0300

    Add project files.

---
+++ BpbonlineTest/Tests/BaseTest.cs
@@ -0,0 +1,16 @@
+namespace BpbonlineTest.Tests
+{
+    public class BaseTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
